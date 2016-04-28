5bc4d532af0998d02a5515713467d509e24ab10c
Phil Carns <carns@mcs.anl.gov>
2016-04-28 09:41:06 -0400

    update to match new margo handler conventions

--- src/bake-bulk-rpc.c
+++ src/bake-bulk-rpc.c
@@ -8,24 +8,22 @@
 #include "bake-bulk-rpc.h"
 
 /* service a remote RPC that instructs the server daemon to shut down */
-static void bake_bulk_shutdown_ult(void *_arg)
+static void bake_bulk_shutdown_ult(hg_handle_t handle)
 {
-    hg_handle_t *handle = _arg;
-
     hg_return_t hret;
     struct hg_info *hgi;
     margo_instance_id mid;
 
     printf("Got RPC request to shutdown\n");
 
-    hgi = HG_Get_info(*handle);
+    hgi = HG_Get_info(handle);
     assert(hgi);
     mid = margo_hg_class_to_instance(hgi->hg_class);
 
-    hret = margo_respond(mid, *handle, NULL);
+    hret = margo_respond(mid, handle, NULL);
     assert(hret == HG_SUCCESS);
 
-    HG_Destroy(*handle);
+    HG_Destroy(handle);
 
     /* NOTE: we assume that the server daemon is using
      * margo_wait_for_finalize() to suspend until this RPC executes, so there
