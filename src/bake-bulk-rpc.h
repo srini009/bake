7118521f73e8bdb9b0ec8b6455bc0bba8139599c
Phil Carns <carns@mcs.anl.gov>
2016-07-31 07:48:36 -0400

    use experimental mercury hook to decode in place

--- src/bake-bulk-rpc.h
+++ src/bake-bulk-rpc.h
@@ -132,15 +132,9 @@ static inline hg_return_t hg_proc_bake_bulk_eager_write_in_t(hg_proc_t proc, voi
     hg_proc_uint64_t(proc, &in->region_offset);
     hg_proc_uint32_t(proc, &in->size);
     if(hg_proc_get_op(proc) == HG_DECODE)
-    {
-        in->buffer = malloc(in->size);
-    }
-    hg_proc_memcpy(proc, in->buffer, in->size);
-    if(hg_proc_get_op(proc) == HG_FREE)
-    {
-        if(in->buffer)
-            free(in->buffer);
-    }
+        hg_proc_memcpy_decode_in_place(proc, &in->buffer, in->size);
+    else
+        hg_proc_memcpy(proc, in->buffer, in->size);
 
     return(HG_SUCCESS);
 }
